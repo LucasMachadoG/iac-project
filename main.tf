b9977e079e9f98fad1d5696d8a30dd4e2a447254
LucasMachadoG <lucasgaelzermachado@gmail.com>
2025-08-06 09:20:55 -0300

    modularize cloudfront

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ module "s3" {
 
   s3_bucket_name = "iac-project"
 }
+
+module "cloudfront" {
+  source             = "./modules/cloudfront"
+  origin_id          = module.s3.bucket_id
+  bucket_domain_name = module.s3.bucket_domain_name
+
+  depends_on = [
+    module.s3
+  ]
+}
---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "origin_id" {
+  type = string
+  description = "Bucket id"
+}
+
+variable "bucket_domain_name" {
+  type = string
+  description = "Bucket domain name"
+}
