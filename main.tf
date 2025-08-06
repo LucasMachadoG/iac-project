64b8c19ce882a3659e297bfdd44fa80c41e3f826
LucasMachadoG <lucasgaelzermachado@gmail.com>
2025-08-06 10:56:32 -0300

    Using a external module

--- main.tf
+++ main.tf
@@ -2,14 +2,32 @@ module "s3" {
   source = "./modules/s3"
 
   s3_bucket_name = "iac-project"
+  s3_tags = {
+    Iac = true
+  }
 }
 
 module "cloudfront" {
   source             = "./modules/cloudfront"
+
   origin_id          = module.s3.bucket_id
   bucket_domain_name = module.s3.bucket_domain_name
+  cdn_price_class    = "PriceClass_200"
+  cdn_tags = {
+    Iac = true
+  }
 
   depends_on = [
     module.s3
   ]
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+
+  name = "iac-sqs"
+  create_dlq = true
+  tags = {
+    Iac = true
+  }
 }
--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -7,3 +7,15 @@ variable "bucket_domain_name" {
   type = string
   description = "Bucket domain name"
 }
+
+variable "cdn_price_class" {
+  type = string
+  default = "PriceClass_200"
+  description = "CDN Price Class"
+}
+
+variable "cdn_tags" {
+  type = map(string)
+  default = {}
+  description = "Creative Tags"
+}
